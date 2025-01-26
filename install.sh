097a8406752ea2bc7ebfae3c26feea989c1d990d
281developer <dave.cowden@gmail.com>
2025-01-26 18:48:31 -0500

    add more image headroom

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ apt-get install -y curl avahi-daemon cpufrequtils v4l-utils libatomic1
 #stuff to install python 3.11.11
 apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 apt-get install -y libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
-apt-get install -y ffmpeg libsm6 libxext6 dos2unix
+apt-get install -y ffmpeg libsm6 libxext6 dos2unix ifstat
 
 
 debug "Setting cpufrequtils to performance mode"
